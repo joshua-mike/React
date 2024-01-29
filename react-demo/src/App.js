65dcd972cf9869059ba18f75f26589e6e303d08f
Josh May <josh.may.one@gmail.com>
2024-01-28 17:51:49 -0800

    Initial commit

---
+++ react-demo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Greet } from './components/Greeting'
+
+function App() 
+{
+  return (
+    <div className="App">
+      <Greet name = ' is dope.' />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-demo/src/components/Greeting.js
@@ -0,0 +1,4 @@
+export const Greet = (props) =>
+{
+    return <h1>React.js Component {props.name}</h1>
+}
