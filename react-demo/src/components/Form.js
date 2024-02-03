51962dca672179134563ddb813b396336bd87122
Josh May <josh.may.one@gmail.com>
2024-02-03 14:37:00 -0800

    Form update

--- react-demo/src/components/Form.js
+++ react-demo/src/components/Form.js
@@ -1,38 +1,51 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const Form = () => 
 {
-    const [country, setCountry] = useState('');
-    const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-    const handleSelection = (event) => 
-    {
-        event.preventDefault();
-        if (event.currentTarget.id === 'countries')
-            alert(`Country selection is: ${country}`);
-        else
-            alert(`Username: ${username}, set.`)
-    };
+  const handleSelection = (event) => 
+  {
+    event.preventDefault();
+    if (username === "")
+        alert(`No user name, no country. Please set a country.`)
+    else alert(`You're golden ${username}. Contry selection, ${event.target.value}, is set.`)
+    // if (event.currentTarget.id === "countries")
+    //   alert(`Country selection is: ${event.target.value}`);
+    // else alert(`You're golden ${username}.`);
+  };
 
-    return (
-<div>
-    <form id="countries" onSubmit={handleSelection}>     
-        <select name="countries" id="country" value={country} onChange={(event) => {setCountry(event.target.value)}}>
-            <option value="Isreal">Israel</option>
-            <option value="Syria">Syria</option>
-            <option value="Labanon">Labanon</option>
-            <option value="Iran">Iran</option>
-            <option value="Iraq">Iraq</option>
-        </select>
-        <button type="submit">Submit</button>
-    </form>    
-    
-    <form id="user" onSubmit={handleSelection}>
+  return (
+    <div>
+        <textarea defaultValue="Enter your username and desired country destination."></textarea>
+      <form id="user">
         <label>Username</label>
-         <input type="text" value={username} onChange={(event) => {setUsername(event.target.value)}}/>     
-        <button type="submit">Submit</button>
-    </form>
-</div>
-)
-}
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => 
+          {
+            setUsername(event.target.value);
+          }}
+        />
+      </form>
 
+      <select
+        name="selection"
+        id="countries"
+        onChange={(event) => 
+        {
+          handleSelection(event);
+        }}
+      >
+        <option value="United States">United States</option>
+        <option value="Mexico">Mexico</option>
+        <option value="Canada">Canada</option>
+        <option value="Croatia">Croatia</option>
+        <option value="Italy">Italy</option>
+        <option value="Greece">Greece</option>
+        <option value="Malta">Malta</option>
+      </select>
+    </div>
+  );
+};
